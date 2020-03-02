37f3130fba1cbc59d7b4877ee4390139c5c8fc37
Robert Wang <robert.lu.wang@gmail.com>
2020-03-02 13:08:30 -0500

    v1.6 add host ping -p and -f filename

--- sysmon.bash
+++ sysmon.bash
@@ -5,7 +5,43 @@
 # v1.2 supports kpi loop  Feb 22, 2020 
 # v1.3 add alarm, email functions  Feb 22, 2020 
 # v1.4 add sqlite3 alarmdb Feb 23, 2020 
-# v1.5 add getopts for cli  Feb 29, 2020 
+# v1.5 add getopts for cli  Feb 29, 2020
+# v1.6 add host ping -p and -f filename  March 1st, 2020
+
+## common setting
+
+source ./sysmon-kpi.sh
+
+DEBUG=1  # 1 - keep outlog    0 - remove outlog 
+readonly LOGFOLDER=`pwd`/.sysmon/outlog  # it must be at shared FS , accessable by all nodes
+readonly REPFOLDER=`pwd`/.sysmon/report  # it must be at shared FS , accessable by all nodes
+
+[ ! -d "$LOGFOLDER" ] && sudo mkdir -p $LOGFOLDER
+sudo chmod 666 $LOGFOLDER
+
+[ ! -d "$REPFOLDER" ] && sudo mkdir -p $REPFOLDER
+sudo chmod 666 $REPFOLDER
+
+# email setting
+EMAILYES=0 # 1 - send email  0 - not send email 
+
+EMAIL_LIST="demo@sysmon.com"
+EMAIL_FILTER="HIGH"
+readonly REPORT_TS=` date '+%m-%d-%Y-%H-%M-%S'`
+EMAIL_SUB="System monitoring report - ${REPORT_TS}"
+readonly REPORT=$REPFOLDER/report_${REPORT_TS}.log
+
+# node access 
+USERNAME='sysmon'
+PASSWORD='sysmon'
+readonly SSHKEY="~/.ssh/id_rsa"
+
+# alarm db setting
+readonly DBPATH="."
+readonly ALARMDB="alarm.db"
+readonly DBDUMP="alarmdb_dump.txt-$REPORT_TS"
+ALARMID=1000000
+readonly SQLITECONN="sqlite3 $DBPATH/$ALARMDB"
 
 lowcase()
 {
@@ -85,60 +121,7 @@ _cmd="$4"
 _kpi="$5"
 _ts="$6"
 
-ssh -i ${_key} -o StrictHostKeyChecking=no ${_username}@${_server} -C "${_cmd}" > $LOGFOLDER/${_kpi}_${_server}_${_ts}.log
-
-}
-
-kpi_fs()
-{
-local _server _threshold _ts usep partition
-
-_server="$1"
-_threshold="$2"
-_ts="$3"
-
-cat $LOGFOLDER/fs_${_server}_${_ts}.log | while read output;
-do
-    usep=$(echo $output | awk '{ print $5}' | cut -d'%' -f1  )
-    partition=$(echo $output | awk '{ print $6 }' )
-    # echo $usep ${_threshold} 
-    [[ $usep -ge ${_threshold} ]] && alarm ${_ts} ${_server} "fs" "high" "$output"
-done
-
-}
-
-kpi_mem()
-{
-local _server _threshold _ts mem_total mem_used mem_free mem_usedp
-
-_server="$1"
-_threshold="$2"
-_ts="$3"
-
-cat $LOGFOLDER/mem_${_server}_${_ts}.log | while read output;
-do
-    mem_total=$(echo $output | awk '{printf("%.0f", $2/1024)}')
-    mem_used=$(echo $output | awk '{printf("%.0f", $3/1024)}')
-    mem_free=$(echo $output | awk '{printf("%.0f", $4/1024)}')
-    mem_usedp=$(echo $output | awk '{printf("%.0f", $3*100/$2)}')
-    [[ "$mem_usedp" > "${_threshold}" ]] && alarm ${_ts} ${_server} "mem" "high" "memory total: ${mem_total}GB memory used: ${mem_used}GB memory free: ${mem_free}GB memory usage: ${mem_usedp}%"
-done
-
-}
-
-kpi_cpu()
-{
-local _server _threshold _ts cpu_usedp
-
-_server="$1"
-_threshold="$2"
-_ts="$3"
-
-cat $LOGFOLDER/cpu_${_server}_${_ts}.log | while read output;
-do
-    cpu_usedp=$(echo $output | awk '{usage=($2+$4)*100/($2+$4+$5)} END {printf ("%.0f\n", usage)}')
-    [[ "$cpu_usedp" > "${_threshold}" ]] && alarm ${_ts} ${_server} "cpu" "high" "cpu usage(>${_threshold}%): ${cpu_usedp}%"
-done
+ssh -n -i ${_key} -o StrictHostKeyChecking=no ${_username}@${_server} -C "${_cmd}" > $LOGFOLDER/${_kpi}_${_server}_${_ts}.log
 
 }
 
@@ -197,52 +180,7 @@ lines=`cat ${_body} | grep "${_filter}" | wc -l | awk '{print $1}'`
 [[ "$lines" -gt 0 ]] && /usr/bin/mailx -s "${_subject}" "${_list}" < ${_body} ; echo "Email already sent out to ${_list}"
 }
 
-## common setting
-
-DEBUG=1  # 1 - keep outlog    0 - remove outlog 
-readonly LOGFOLDER=`pwd`/.sysmon/outlog  # it must be at shared FS , accessable by all nodes
-readonly REPFOLDER=`pwd`/.sysmon/report  # it must be at shared FS , accessable by all nodes
-
-[ ! -d "$LOGFOLDER" ] && sudo mkdir -p $LOGFOLDER
-sudo chmod 666 $LOGFOLDER
-
-[ ! -d "$REPFOLDER" ] && sudo mkdir -p $REPFOLDER
-sudo chmod 666 $REPFOLDER
-
-# email setting
-EMAILYES=0 # 1 - send email  0 - not send email 
-
-EMAIL_LIST="demo@sysmon.com"
-EMAIL_FILTER="HIGH"
-readonly REPORT_TS=` date '+%m-%d-%Y-%H-%M-%S'`
-EMAIL_SUB="System monitoring report - ${REPORT_TS}"
-readonly REPORT=$REPFOLDER/report_${REPORT_TS}.log
-
-# node access 
-USERNAME='sysmon'
-PASSWORD='sysmon'
-readonly SSHKEY="~/.ssh/id_rsa"
-
-# kpi common setting
-KPIFULL="FS MEM CPU"
-readonly CMD_FS="df -H | grep -vE '^Filesystem|tmpfs|cdrom|boot'|grep %"
-readonly CMD_MEM="free -m|grep Mem"
-readonly CMD_CPU="grep '^cpu ' /proc/stat"
-THRESHOLD_FS=50
-THRESHOLD_MEM=20
-THRESHOLD_CPU=5
-
-# alarm db setting
-readonly DBPATH="."
-readonly ALARMDB="alarm.db"
-readonly DBDUMP="alarmdb_dump.txt-$REPORT_TS"
-ALARMID=1000000
-readonly SQLITECONN="sqlite3 $DBPATH/$ALARMDB"
-
-## loop body to process kpi monitor and alarm per pki on servers 
-
 # localhost monitor 
-
 local_monitor()
 {
 TS=` date '+%m-%d-%Y-%H-%M-%S'`
@@ -265,27 +203,46 @@ echo >> $REPORT
 
 }
 
+# remote monitor with ssh key
 remote_monitor_key()
 {
-local _server
+local _server node
 _server="$1"
 
 TS=` date '+%m-%d-%Y-%H-%M-%S'`
-for node in ${_server}
-do
-    for kpi in $KPI
-    do 
-        echo >> $REPORT
-        echo "`date ` - $node - $kpi"  >> $REPORT
-        echo >> $REPORT
-        CMD_NAME=`echo CMD_$kpi`
-        THRESHOLD_NAME=`echo THRESHOLD_$kpi`
-        CMD=${!CMD_NAME}
-        THRESHOLD=${!THRESHOLD_NAME}
-        ssh_run_key "$node" "$USERNAME" "$SSHKEY" "$CMD" "`lowcase $kpi`" "$TS"
-        kpi_`lowcase $kpi` "$node" "$THRESHOLD" "$TS" >> $REPORT
-    done 
-done
+if [ -n "$FILE" ];then # node list file
+    cat ${_server} | while read node;
+    do
+        for kpi in $KPI
+        do 
+            echo >> $REPORT
+            echo "`date ` - $node - $kpi"  >> $REPORT
+            echo >> $REPORT
+            CMD_NAME=`echo CMD_$kpi`
+            THRESHOLD_NAME=`echo THRESHOLD_$kpi`
+            CMD=${!CMD_NAME}
+            THRESHOLD=${!THRESHOLD_NAME}
+            ssh_run_key "$node" "$USERNAME" "$SSHKEY" "$CMD" "`lowcase $kpi`" "$TS"
+            kpi_`lowcase $kpi` "$node" "$THRESHOLD" "$TS" >> $REPORT
+        done 
+    done
+else # node list string
+    for node in ${_server}
+    do
+        for kpi in $KPI
+        do 
+            echo >> $REPORT
+            echo "`date ` - $node - $kpi"  >> $REPORT
+            echo >> $REPORT
+            CMD_NAME=`echo CMD_$kpi`
+            THRESHOLD_NAME=`echo THRESHOLD_$kpi`
+            CMD=${!CMD_NAME}
+            THRESHOLD=${!THRESHOLD_NAME}
+            ssh_run_key "$node" "$USERNAME" "$SSHKEY" "$CMD" "`lowcase $kpi`" "$TS"
+            kpi_`lowcase $kpi` "$node" "$THRESHOLD" "$TS" >> $REPORT
+        done 
+    done
+fi
 
 echo >> $REPORT
 
@@ -293,47 +250,102 @@ echo >> $REPORT
 
 }
 
+# remote monitor with user/pass
 remote_monitor_pass()
 {
-local _server
+local _server node
 _server="$1"
 
 TS=` date '+%m-%d-%Y-%H-%M-%S'`
 
-for node in ${_server}
-do
-    for kpi in $KPI
-    do 
-        echo >> $REPORT
-        echo "`date ` - $node - $kpi"  >> $REPORT
-        echo >> $REPORT
-        CMD_NAME=`echo CMD_$kpi`
-        THRESHOLD_NAME=`echo THRESHOLD_$kpi`
-        CMD=${!CMD_NAME}
-        THRESHOLD=${!THRESHOLD_NAME}
-        ssh_run_pass "$node" "$USERNAME" "$PASSWORD" "$CMD" "`lowcase $kpi`" "$TS"
-        kpi_`lowcase $kpi` "$node" "$THRESHOLD" "$TS" >> $REPORT
-    done 
-done
+if [ -n "$FILE" ];then  # node list file
+    cat ${_server} | while read node;
+    do
+        for kpi in $KPI
+        do 
+            echo >> $REPORT
+            echo "`date ` - $node - $kpi"  >> $REPORT
+            echo >> $REPORT
+            CMD_NAME=`echo CMD_$kpi`
+            THRESHOLD_NAME=`echo THRESHOLD_$kpi`
+            CMD=${!CMD_NAME}
+            THRESHOLD=${!THRESHOLD_NAME}
+            ssh_run_pass "$node" "$USERNAME" "$PASSWORD" "$CMD" "`lowcase $kpi`" "$TS"
+            kpi_`lowcase $kpi` "$node" "$THRESHOLD" "$TS" >> $REPORT
+        done 
+    done
+else # node list string
+    for node in ${_server}
+    do
+        for kpi in $KPI
+        do 
+            echo >> $REPORT
+            echo "`date ` - $node - $kpi"  >> $REPORT
+            echo >> $REPORT
+            CMD_NAME=`echo CMD_$kpi`
+            THRESHOLD_NAME=`echo THRESHOLD_$kpi`
+            CMD=${!CMD_NAME}
+            THRESHOLD=${!THRESHOLD_NAME}
+            ssh_run_pass "$node" "$USERNAME" "$PASSWORD" "$CMD" "`lowcase $kpi`" "$TS"
+            kpi_`lowcase $kpi` "$node" "$THRESHOLD" "$TS" >> $REPORT
+        done 
+    done
+fi
 
 echo >> $REPORT
 
 [[ $DEBUG -ne 1 ]] && rm -f $LOGFOLDER/*_$TS.log || echo "Please see detail log at ""$LOGFOLDER/<kpi>_<node>_$TS.log" >> $REPORT
 
+}
+
+host_ping()
+{
+local _server node
+_server="$1"
+
+TS=` date '+%m-%d-%Y-%H-%M-%S'`
+
+if [ -n "$FILE" ];then  # node list file
+    cat ${_server} | while read node;
+    do 
+        ping -c 3 $node > /dev/null 2>&1
+        if [[ $? -ne 0 ]];then
+            echo "Server $node : down" >> $LOGFOLDER/ping_$node_$TS.log
+            alarm $TS $node "ping" "high" "Server $node : down"
+        else
+            echo "Server $node : up" >> $LOGFOLDER/ping_$node_$TS.log
+            echo $TS " " $node " ping - [ Server " $node " : up  ]" 
+        fi
+    done
+else  # node list string
+    for node in ${_server}
+    do
+        ping -c 3 $node > /dev/null 2>&1
+        if [[ $? -ne 0 ]];then
+            echo "Server $node : down" >> $LOGFOLDER/ping_$node_$TS.log
+            alarm $TS $node "ping" "high" "Server $node : down"
+        else
+            echo "Server $node : up" >> $LOGFOLDER/ping_$node_$TS.log
+            echo $TS " " $node " ping - [ Server " $node " : up  ]" 
+        fi
+    done
+fi
 }
 
 ## main loop 
 
-#SERVER=`cat /etc/hosts|grep localhost|grep -v ip6|awk '{print $2}'|sort`
+unset SERVER FILE KPI MODE 
 
-unset SERVER KPI MODE
+PING=0
 
 usage()
 {
-    echo "Usage: $0 -s [localhost|server] -k [all|kpi] [-m key|pass] [-l] [-h]"
-    echo "-s server name, localhost needs -k option; remote server needs -k -m options"
+    echo "Usage: $0 [-s localhost|server] [-f serverfile] [-k all|kpi] [-m key|pass] [-p] [-l] [-h]"
+    echo "-s server string, localhost needs -k option; remote server needs -k -m options for system monitor or needs -p for ping test"
+    echo "-f server filename, cannot exist with -s at sametime; need -k -m options for system monitor or need -p for ping test"
     echo "-k kpi name, all or valid kpi name like fs, mem and cpu etc"
     echo "-m access mode, ssh remote access with key or user/password"
+    echo "-p ping host, need to work with -s or -f option"
     echo "-l list available kpi list"
     echo "-h help"
     exit
@@ -343,11 +355,17 @@ if [ $# -eq 0 ]; then
     usage
 fi
 
-while getopts ":s:k:m:lh" opt; do
+while getopts ":s:f:k:m:plh" opt; do
 case $opt in
     s) SERVER="$OPTARG"
     if [[ -z "$SERVER" ]];then 
-        echo Cannot be empty for server
+        echo "Cannot be empty for server"
+        usage
+    fi
+    ;;
+    f) FILE="$OPTARG"
+    if [[ -z "$FILE" ]];then 
+        echo "Cannot be empty for server file"
         usage
     fi
     ;;
@@ -369,6 +387,8 @@ case $opt in
         usage
     fi
     ;;
+    p) PING=1
+    ;;
     l) echo `lowcase "$KPIFULL"`
     exit
     ;;
@@ -380,34 +400,112 @@ case $opt in
 esac
 done
 
-if [[ -z $SERVER ]];then
-    echo "-s is must option"
+## rules for options check 
+
+if [[ $PING -eq 1 ]];then 
+    if [[ -n $KPI || -n $MODE ]];then
+        echo "1 No -k and -m needed for ping test"
+        usage
+    elif [[ -z $FILE ]] && [[ -z $SERVER ]];then
+        echo "2 -s|-f needed for ping test"
+        usage
+    elif [[ -n $FILE ]] && [[ -n $SERVER ]];then
+        echo "3 Cannot have -s and -f at sametime"
+        usage
+    fi
+fi
+
+if [[ $PING -eq 0 ]] && [[ -n $KPI ]] && [[ -n $MODE ]];then
+    if [[ -z $SERVER ]] && [[ -z $FILE ]];then
+        echo "4 -s|-f needed for system monitor"
+        usage
+    fi
+fi
+
+if [[ $PING -eq 0 && -n $KPI && -z $MODE && ( -n $SERVER || -n $FILE ) ]];then
+    if [[ "$SERVER" == "localhost" ]];then
+        :
+    else
+        echo "5 -m needed for remote access"
+        usage
+    fi
+fi
+
+if [[ $PING -eq 0 ]] && [[ "$SERVER" == "localhost" ]];then
+    if [[ -n $MODE ]];then
+        echo "6 no -m needed for localhost monitor"
+        usage
+    elif [[ -n $FILE ]];then 
+        echo "7 Cannot have -f with -s at sametime"
+        usage
+    elif [[ -z $KPI ]];then 
+        echo "8 -k needed for localhost monitor"
+        usage
+    fi
+fi
+
+if [[ $PING -eq 0 && -z $KPI && -n $MODE && ( -n $SERVER || -n $FILE ) ]];then
+    echo "9 -k needed for system monitor"
     usage
 fi
 
-if [[ -z $KPI ]];then
-    echo "-k is must option"
+if [[ $PING -eq 0 && -z $KPI && -z $MODE && ( -n $SERVER || -n $FILE ) ]];then
+    echo "10 -k and -m needed for system monitor"
     usage
 fi
 
-if [[ $SERVER != "localhost" ]] && [[ -z $MODE ]]
-then
-    echo "-m is must option for remote server"
-    usage
+if [[ $PING -eq 0 && -n $KPI ]];then
+    if [[ "$SERVER" == "localhost" ]];then
+        :
+    elif [[ -z $MODE || ( -z $SERVER && -z $FILE ) ]];then
+        echo "11 -m or -s|-f needed for system monitor"
+        usage
+    fi
 fi
 
-if [[ $SERVER == "localhost" ]] && [[ -n $MODE ]]
-then
-    echo "Don't need -m for localhost"
-    usage
+if [[ $PING -eq 0 && -n $MODE ]];then
+    if [[ -z $KPI || ( -z $SERVER && -z $FILE ) ]];then
+        echo "12 -k or -s|-f needed for system monitor"
+        usage
+    fi
 fi
 
-if [[ $SERVER == "localhost" ]];then
-    local_monitor
-elif [[ $MODE == "KEY" ]]; then
-    remote_monitor_key "$SERVER"
-elif [[ $MODE == "PASS" ]]; then 
-    remote_monitor_pass "$SERVER"
+
+## host ping test
+if [[ $PING -eq 1 ]];then 
+    if [[ -n $FILE ]];then
+        host_ping "$FILE"
+        exit
+    else
+        host_ping "$SERVER"
+        exit
+    fi
+fi
+
+## system monitor task 
+
+if [[ $PING -eq 0 && -n $SERVER && -z $FILE ]];then
+    if [[ -n $KPI && -z $MODE ]];then
+        if [[ "$SERVER" == "localhost" ]];then
+            local_monitor
+        fi 
+    fi
+fi
+
+if [[ $PING -eq 0 && -n $KPI && -n $MODE && ( -n $SERVER || -n $FILE ) ]];then
+    if [[ $MODE == "KEY" ]]; then
+        if [[ -n $FILE ]];then
+            remote_monitor_key "$FILE"
+        else
+            remote_monitor_key "$SERVER"
+        fi
+    elif [[ $MODE == "PASS" ]]; then 
+        if [[ -n $FILE ]];then
+            remote_monitor_pass "$FILE"
+        else
+            remote_monitor_pass "$SERVER"
+        fi
+    fi
 fi
 
 # send report by email 
@@ -419,6 +517,9 @@ echo The system monitoring report saved at $REPORT
 echo
 
 [[ $EMAILYES -eq 1 ]] && send_email "${EMAIL_SUB}" "$REPORT" "${EMAIL_FILTER}" "${EMAIL_LIST}"
+
+
+
 
 
 
